b42754d62d59f0e3d26956eea0d398230885f3de
PragmaCodeRepo <chaubeypranjal027@gmail.com>
2024-04-18 15:21:21 +0530

    18 April 2024-4

--- app/settings.py
+++ app/settings.py
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'radioai',
-        'USER': 'pranjal',
-        'PASSWORD': '$GroundRushLabs!$',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'radioaidb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'pass@123',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
